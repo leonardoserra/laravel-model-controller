8cbc88c3f8c44ffabc18083ae02569bfe4d27272
leonardoserra <serra.leonardo92@gmail.com>
2023-05-11 16:57:01 +0200

    creata navbar, creato model con controller  Movie

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+namespace App\Http\Controllers\Guest;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[PageController::class, 'index']);
