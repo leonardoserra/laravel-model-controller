67a8b90e152c0627865aaa80d886e01e1bda389e
leonardoserra <serra.leonardo92@gmail.com>
2023-05-11 17:14:16 +0200

    ciclate card con foreach e bootstrap

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,15 +3,33 @@
 @section('page-title','Homepage')
 
 @section('content')
-
-        <ul>
-        @foreach ($movies as $movie )
-
-        <li>
-            {{$movie}}
-        </li>
-            
-        @endforeach
-        </ul>
+    <div class="container">
+        <div class="row">
+            @foreach ($movies as $movie )
+            <div class="col-3 mb-5">
+                <div class="card" style="width: 18rem;">
+                    <div class="card-header">
+                        Titolo: {{$movie['title']}}
+                    </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">
+                        Titolo originale: {{$movie['original_title']}}
+                    </li>
+                    <li class="list-group-item">
+                       Nazionalitá: {{$movie['nationality']}}
+                    </li>
+                    <li class="list-group-item">
+                       Data: {{$movie['date']}}
+                    </li>
+                </ul>
+                    <div class="card-footer">
+                       Voto: {{$movie['vote']}}
+                    </div>
+                </div>
+            </div>
+            @endforeach
+        </div>
+    </div>
+        
 
 @endsection
